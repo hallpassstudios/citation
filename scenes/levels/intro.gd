e5403cbf3fa3a86031ac95023cfe17a46457edb9
xtian <christian@nyu.edu>
2021-09-08 13:40:46 -0400

    minor changes

--- scenes/levels/intro.gd
+++ scenes/levels/intro.gd
@@ -1,6 +1,13 @@
 extends Control
 
 var current_animation = 0
+var timer
+
+var page_1 : Label
+var page_2 : Label
+var page_3 : Label
+var page_4 : Label
+
 
 func _ready():
 	next_page()
@@ -8,11 +15,18 @@ func _ready():
 func _on_AnimatedSprite_animation_finished():
 	current_animation += 1
 	# wait a fixed amount of time and then go to the next page
-	next_page()
 	
+	# play a loop animation
+	$AnimatedSprite.play("cycle 1")
 	
 func next_page():
-	$AnimatedSprite.play(str(current_animation))
+	
+	match current_animation:
+		1:
+			print("here's page 1")
+			$AnimatedSprite.play(str(current_animation))
+	
 
 func _on_Button_pressed():
 	next_page()
+
